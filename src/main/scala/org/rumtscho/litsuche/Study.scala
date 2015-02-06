374a9802628aa595c6bd553a557a9041e105ec8d
rumtscho <rumtscho@gmail.com>
2015-02-06 17:57:00 +0100

    Added the new files to the git repo too

---
+++ src/main/scala/org/rumtscho/litsuche/Study.scala
@@ -0,0 +1,7 @@
+package org.rumtscho.litsuche
+
+class Study(val id: Int = -1, 
+    val handmadeAuthorRef: String = "empty") 
+{
+
+}
---
+++ src/main/scala/org/rumtscho/litsuche/VariableAppearance.scala
@@ -0,0 +1,9 @@
+package org.rumtscho.litsuche
+
+class VariableAppearance(
+    val varId: Int = -1, 
+    val varName: String = ""
+    )
+    {
+
+}
